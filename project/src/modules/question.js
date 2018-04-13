556cccf9f70c798ffa839330e107340048fec5c7
Tomoya Mikami <trymorecc@gmail.com>
2018-04-13 13:04:24 +0900

    modulesの全削除

--- project/src/modules/question.js
+++
@@ -1,6 +0,0 @@
-// 問題を生成するモジュール
-
-export default function create_question(id) {
-
-    // id 参照するhtmlのタグ
-}
--- project/src/modules/timer.js
+++
@@ -1,9 +0,0 @@
-// itmerを設定するモジュール
-
-export default function start_time() {
-    return new Date().getTime();
-}
-
-export default function end_time(start_time) {
-    return new Data().getTime() = start_time;
-}
