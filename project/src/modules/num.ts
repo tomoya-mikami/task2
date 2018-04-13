2ca00e314d1c6258f03e42c7d304ea3e5d11e727
Tomoya Mikami <trymorecc@gmail.com>
2018-04-13 16:48:04 +0900

    環境リセット

--- project/src/modules/num.ts
+++
@@ -1,5 +0,0 @@
-export default function Getrandom(start: number, end: number){
-    let random : number = Math.floor(Math.random() * (start + 1 - end)) + end;
-
-    return random;
-}
--- project/src/modules/question.ts
+++
@@ -1,9 +0,0 @@
-export class Question {
-    public quesion_size : number;
-    public filename : string[];
-
-    constructor (question_size : number, filename : string[]) {
-        this.quesion_size = question_size;
-        this.filename = filename;
-    }
-}
